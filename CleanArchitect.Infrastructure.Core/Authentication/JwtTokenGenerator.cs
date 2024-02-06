2cdfd194b5af6afc9bff83ac6e36be15b7abd710
Hadi-a20 <hadiardebili@gmail.com>
2024-02-06 22:23:05 +0330

    Implemented JwtTokenGenerator.

--- CleanArchitect.Infrastructure.Core/Authentication/JwtTokenGenerator.cs
+++ CleanArchitect.Infrastructure.Core/Authentication/JwtTokenGenerator.cs
@@ -1,12 +1,43 @@
 ﻿using CleanArchitect.Application.Common.Services.Authentication;
 using CleanArchitect.Domain.Users;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace CleanArchitect.Infrastructure.Core.Authentication;
 
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(IOptions<JwtSettings> jwtOptiones)
+    {
+        _jwtSettings = jwtOptiones.Value;
+    }
+
     public string GenerateToken(User user)
     {
-        return "token";
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+            SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: DateTime.Now.AddMinutes(_jwtSettings.ExpireInMinutes),
+            claims: claims,
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
     }
 }
